2f897b969e65a9d9ca50bd0e02853f390e2c6738
ZHB <zhb020@gmail.com>
2017-10-09 00:31:18 +0800

    修改

--- src/util/constant.js
+++ src/util/constant.js
@@ -2,6 +2,6 @@ export default {
     host: 'http://localhost:8080',
     // host: 'http://api.chuangshi.nowui.com',
     index: '/00/index',
-    index: '/01/index',
-    index: '/02/index'
+    // index: '/01/index',
+    // index: '/02/index'
 };
--- src/view/00/Index.js
+++ src/view/00/Index.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react';
 import {connect} from 'dva';
 import {routerRedux} from 'dva/router';
+import {Modal} from 'antd';
 import Slider from 'react-slick';
 
 class Index extends Component {
@@ -8,7 +9,7 @@ class Index extends Component {
         super(props);
 
         this.state = {
-
+            visible: true
         }
     }
 
@@ -29,33 +30,43 @@ class Index extends Component {
         this.slider.slickPrev();
     }
 
+    handleOk() {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleCancel() {
+        this.setState({
+            visible: false,
+        });
+    }
+
     render() {
         return (
             <div className="index-00-bg">
-                <div className="bottom-position">
-                    <div className="bottom-carousel">
-                        <Slider ref={c => this.slider = c } {...{
-                            infinite: true,
-                            speed: 500,
-                            slidesToScroll: 4,
-                            slidesToShow: 4,
-                            arrows: false
-                        }}>
-                            <div className="bottom-li"><img src={require('../../image/index_00_but01.png')} /></div>
-                            <div className="bottom-li"><img src={require('../../image/index_00_but01.png')} /></div>
-                            <div className="bottom-li"><img src={require('../../image/index_00_but01.png')} /></div>
-                            <div className="bottom-li"><img src={require('../../image/index_00_but01.png')} /></div>
-                        </Slider>
+                <div className="index-00-carousel">
+                    <Slider ref={c => this.slider = c } {...{
+                        infinite: true,
+                        speed: 500,
+                        slidesToScroll: 4,
+                        slidesToShow: 4,
+                        arrows: false
+                    }}>
+                        <div className="index-00-carousel-item"><img src={require('../../image/index_00_but01.png')} /></div>
+                        <div className="index-00-carousel-item"><img src={require('../../image/index_00_but01.png')} /></div>
+                        <div className="index-00-carousel-item"><img src={require('../../image/index_00_but01.png')} /></div>
+                        <div className="index-00-carousel-item"><img src={require('../../image/index_00_but01.png')} /></div>
+                    </Slider>
 
-                    </div>
-                    <div>
-                        <span className='button_previous' onClick={this.handlePrevious.bind(this)}>
+                </div>
+                <div>
+                        <span className='index-00-carousel-previous' onClick={this.handlePrevious.bind(this)}>
                             <img src={require('../../image/index_00_pre.png')} />
                         </span>
-                        <span className='button_next' onClick={this.handleNext.bind(this)}>
+                        <span className='index-00-carousel-next' onClick={this.handleNext.bind(this)}>
                             <img src={require('../../image/index_00_next.png')} />
                         </span>
-                    </div>
                 </div>
 
                 <div className="side">
@@ -67,32 +78,43 @@ class Index extends Component {
                         <p>登陆成功的党员</p>
                         <ul>
                             <li>
-                                <span className="uner_img"><img src={require('../../image/index_00_uimg01.png')} /></span>
+                                <img className="member-image" src={require('../../image/index_00_uimg01.png')} alt="" />
                                 <span>微信名字</span>
                             </li>
                             <li>
-                                <span className="uner_img"><img src={require('../../image/index_00_uimg02.png')} /></span>
+                                <img className="member-image" src={require('../../image/index_00_uimg01.png')} alt="" />
                                 <span>微信名字</span>
                             </li>
                             <li>
-                                <span className="uner_img"><img src={require('../../image/index_00_uimg03.png')} /></span>
+                                <img className="member-image" src={require('../../image/index_00_uimg01.png')} alt="" />
                                 <span>微信名字</span>
                             </li>
                             <li>
-                                <span className="uner_img"><img src={require('../../image/index_00_uimg01.png')} /></span>
+                                <img className="member-image" src={require('../../image/index_00_uimg01.png')} alt="" />
                                 <span>微信名字</span>
                             </li>
                             <li>
-                                <span className="uner_img"><img src={require('../../image/index_00_uimg02.png')} /></span>
+                                <img className="member-image" src={require('../../image/index_00_uimg01.png')} alt="" />
                                 <span>微信名字</span>
                             </li>
                             <li>
-                                <span className="uner_img"><img src={require('../../image/index_00_uimg03.png')} /></span>
+                                <img className="member-image" src={require('../../image/index_00_uimg01.png')} alt="" />
                                 <span>微信名字</span>
                             </li>
                         </ul>
                     </div>
                 </div>
+                <Modal
+                    title="Basic Modal"
+                    width = {1000}
+                    visible={this.state.visible}
+                    onOk={this.handleOk.bind(this)}
+                    onCancel={this.handleCancel.bind(this)}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
             </div>
         );
     }
