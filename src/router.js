1edc89294a7daf072485d71ff9c5b2047781e41f
ZHB <zhb020@gmail.com>
2017-10-09 23:35:12 +0800

    修改

--- src/router.js
+++ src/router.js
@@ -2,10 +2,10 @@ import React from "react";
 import {Router, Route, IndexRedirect} from "dva/router";
 import Main from "./view/Main";
 import Index0 from "./view/0/Index";
-import Index1 from "./view/1/Index";
 import Index2 from "./view/2/Index";
 import Index3 from "./view/3/Index";
 import Index4 from "./view/4/Index";
+import Index5 from "./view/5/Index";
 
 import constant from "./util/constant";
 
@@ -17,10 +17,10 @@ function RouterConfig({history}) {
                 <IndexRedirect to={'/' + constant.id + '/index'}/>
                 <Route component={Main}>
                     <Route path="/0/index" component={Index0}/>
-                    <Route path="/1/index" component={Index1}/>
                     <Route path="/2/index" component={Index2}/>
                     <Route path="/3/index" component={Index3}/>
                     <Route path="/4/index" component={Index4}/>
+                    <Route path="/5/index" component={Index5}/>
                 </Route>
             </Route>
         </Router>
--- src/util/constant.js
+++ src/util/constant.js
@@ -3,7 +3,11 @@ export default {
     // socket: 'http://api.chuangshi.nowui.com:2998',
     host: 'http://localhost:8080',
     // host: 'http://api.chuangshi.nowui.com',
-    id: 1,
+    id: 0,
+    // id: 2,
+    // id: 3,
+    // id: 4,
+    // id: 5,
     platform: 'H5',
     version: '1.0.0'
 };
