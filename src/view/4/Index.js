9999a2886002d5b97640c7397ababf24fb8fe6fa
nowui <nowui@163.com>
2017-10-18 14:48:07 +0800

    修改

--- src/view/4/Index.js
+++ src/view/4/Index.js
@@ -161,7 +161,7 @@ class Index extends Component {
                                     infinite: true,
                                     variableWidth: true,
                                     speed: 500,
-                                    slidesToShow: 1,
+                                    slidesToShow: 4,
                                     slidesToScroll: 1,
                                     arrows: false
                                 }}>
